c0a209374fc6c4f147d650ee8b3483f7d5973c83
inge4pres <fgualazzi@gmail.com>
2015-01-01 20:35:58 +0100

    handlerr: Completed LogErr method

--- handlerr/handlerr.go
+++ handlerr/handlerr.go
@@ -1,22 +1,12 @@
 package hendlerr
 
 import (
-	"fmt"
-	"io"
 	"log"
 	"os"
 )
 
-const (
-	INFO  = 0
-	WARN  = 1
-	DEBUG = 2
-	FATAL = 3
-)
-
 type Herr struct {
-	LogFile, Descr string
-	Level          int
+	LogFile, Level, Descr string
 	Err            error
 }
 
@@ -24,51 +14,31 @@ func (e *Herr) LogErr() {
 	l, f := initLog(e.LogFile, e.Level)
 	defer f.Close()
 
-	select {
-
-	case e.Level = INFO:
-		l.Println("[INFO] " + e.Descr)
-		l.Printf("Error :%T - %s", err, err)
-
-	case e.Level = WARN:
-		l.Println("[WARN] " + e.Descr)
-		l.Printf("Error :%T - %s", err, err)
-
-	case e.Level = DEBUG:
-
-		l.Println("[DEBUG] " + e.Descr)
-		l.Printf("Error :%T - %s", err, err)
-
-	case e.Level = INFO:
-		l.Println("[FATAL] " + e.Descr)
-		l.Fataltf("Error :%T - %s", err, err)
-	}
-
 	if e.Err != nil {
+		l.Println(e.Descr)
 		l.Printf("Error: %T - %s", e.Err, e.Err)
 	}
 }
 
-func initLog(path string, flag int) (l *log.Logger, file *os.File) {
+func initLog(path, level string) (l *log.Logger, file *os.File) {
 
-	offset = 0
 	if _, err := os.Stat(path); os.IsNotExist(err) {
 		if file, err := os.Create(path); err == nil {
-			l = log.New(file, nil, flag)
-			return
+			l = log.New(file, level, log.Lshortfile)
 		} else {
-			logger := log.New(os.Stdout, nil, log.Llongfile)
+			logger := log.New(os.Stdout, "FATAL", log.Llongfile)
 			logger.Fatalf("Cannot init log file %s\n%T\n%s", path, err, err)
 		}
+	return
 	}
 
 	file, err := os.OpenFile(path, os.O_RDWR, 0755)
 
 	if err != nil {
-		logger := log.New(os.Stdout, nil, log.Llongfile)
+		logger := log.New(os.Stdout, "FATAL", log.Llongfile)
 		logger.Fatalf("Cannot init log file %s\n%T\n%s", path, err, err)
 	}
 
-	l = log.New(file, nil, Lshortfile)
+	l = log.New(file, level, log.Lshortfile)
 	return
 }
