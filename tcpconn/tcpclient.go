27e3e69e3b3a1ec53225f83119a4520dc3813f99
inge4pres <fgualazzi@gmail.com>
2015-01-01 22:32:08 +0100

    tcpconn: tcpclient now usess a buffered channel to handle server response 	 tcpconn refactoring with comments

--- tcpconn/tcpclient.go
+++ tcpconn/tcpclient.go
@@ -5,8 +5,14 @@ import (
 	json "encoding/json"
 	"os"
 	"fmt"
+	"errors"
+	"bytes"
 )
 
+//TODO Add IPv6 support
+/*
+Connect4: starts a IPv4 connection with a remote server, leaves it open for future use 
+*/
 func (comm *TCPCommand) Connect4() (conn *net.TCPConn, ex error) {
 	addr, _ := net.ResolveTCPAddr("tcp4", comm.RHost+":"+comm.RPort)
 	conn, ex = net.DialTCP("tcp", nil, addr)
@@ -17,20 +23,64 @@ func (comm *TCPCommand) Connect4() (conn *net.TCPConn, ex error) {
 	}
 	return
 }
-
-func (comm *TCPCommand) SendCommand(conn *net.TCPConn) {
+/*
+Disconnect: close the previously opened connection
+*/
+func (comm *TCPCommand) Disconnect(conn *net.TCPConn) {
+	conn.Close()
+}
+/*
+PostCommand: send a command on a previously opened connection; command is json-encoded; returns the response in a TcpExData interface
+*/
+func (comm *TCPCommand) PostCommand(conn *net.TCPConn) (ex error) {
+	var init string
 	conn.Write([]byte(START_COMM))
-	buf := make([]byte, MAX_BUFF_SIZE)
+	buf := make([]byte, MAX_COMM_SIZE)
+
 	_, err := conn.Read(buf)
 	if (err != nil) {
 		fmt.Fprintf(os.Stdout, "Error reading from buffer of TCP Connection\n%T\n%s\n", err, err)
-		os.Exit(1)
+		ex = err
+		return
 	}
-	resp := string(buf)
-	if (resp == RESP_OK) {
-		buf = make([]byte, MAX_BUFF_SIZE)
+	init = string(buf)
+	if (init == RESP_OK) {
 		jenc, _ := json.Marshal(comm)
 		conn.Write([]byte(jenc))
+        }else{
+		ex = errors.New("Command was not accepted by remote server")
 	}
+	return ex
 }
-
+/*
+ReceiveResp: receive the response from the remote server
+*/
+func (comm *TCPCommand) ReceiveResp(conn *net.TCPConn) (data *TCPExData, ex error){
+	dt := make(chan []byte)
+	errCh := make(chan error)
+	var resp bytes.Buffer
+	go func(){
+		for {
+			buf := make([]byte, MAX_BUFF_SIZE)
+			_, err := conn.Read(buf)
+			if err != nil {
+				errCh<-err
+			}
+			dt<-buf
+		}
+	}()
+	resp.Write(<-dt)
+	ex = json.Unmarshal(resp.Bytes(), data)
+	return
+}
+/*
+SendData: send data on a previously opened connection; data is json-encoded
+*/
+func (data *TCPExData) SendData(conn *net.TCPConn) (ex error) {
+	_, err := json.Marshal(data.Data)
+	if err != nil {
+		ex = err
+	}
+	//TODO
+	return
+}
--- tcpconn/tcpconn.go
+++ tcpconn/tcpconn.go
@@ -1,19 +1,19 @@
 package tcpconn
 
 const (
-        PHASE_CONN = 0
-        PHASE_AUTH = 1
-        PHASE_SEND = 2
-        PHASE_RETR = 3
-        PHASE_SYNC = 4
+        PHASE_CONN = 0 //Initial connection verification
+        PHASE_AUTH = 1 //Authentication between client and server
+        PHASE_SEND = 2 //Send data to server
+        PHASE_RETR = 3 //Receive data from server
+        PHASE_SYNC = 4 //Sync (optional)
 
-        MAX_FILE_SIZE = 10485760
-        MAX_BUFF_SIZE = 32768
-        WRKR_COUNT = 100
-        MAX_COMM_SIZE = 2048
+        MAX_FILE_SIZE = 10485760  //10MB 
+        MAX_BUFF_SIZE = 32768     //32KB
+        WRKR_COUNT = 100          //Max number of workers for concurrency
+        MAX_COMM_SIZE = 2048      //Max size of Commands
 
-	RESP_OK = "OK"
-	START_COMM = "INIT"
+	RESP_OK = "OK"            //Default initial response, used for connectivity validation
+	START_COMM = "INIT"       //Default initial request, used or connection validation
 )
 
 type TCPCommand struct {
