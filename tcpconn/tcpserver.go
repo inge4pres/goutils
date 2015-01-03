cd30c799cca62ee5b40e992477256fb5f0895547
inge4pres <fgualazzi@gmail.com>
2015-01-03 22:48:31 +0100

    Using gotuils/handlerr Improved connection handling server-side

--- tcpconn/tcpserver.go
+++ tcpconn/tcpserver.go
@@ -1,64 +1,58 @@
-package  tcpconn
+package tcpconn
 
 import (
 	"fmt"
+	ex "inge4pres/goutils/handlerr"
 	"net"
 	"os"
 )
 
-func errorHandler(e error) bool {
-	res := true
-	if(e != nil){
-		res = false
-	}
-	return res
-}
+var e = ex.Herr{LogFile: "./tcpserver.err"}
 
 func TcpServerListener(conntype, host, port string) error {
 	addr, _ := net.ResolveTCPAddr(conntype, host+":"+port)
 	l, err := net.ListenTCP(conntype, addr)
 	defer l.Close()
-	if(errorHandler(err)){
-		fmt.Fprintf(os.Stdout,"Accepting connection on %s://%s:%s\n", conntype, host, port )
-	} else{
+	if ex.IsErr(err) == nil {
+		fmt.Fprintf(os.Stdout, "Accepting connection on %s://%s:%s\n", conntype, host, port)
+	} else {
+		e.HandlErr("FATAL", "Could not open socket for listening, exiting...", err)
 		return err
 	}
 	for {
 		conn, err := l.AcceptTCP()
-		if (!errorHandler(err)){
-			fmt.Fprintf(os.Stdout,"Error occured during connection: %s\n", err)
+		if ex.IsErr != nil {
+			e.HandlErr("WARN", "", err)
 			return err
 		}
-	go handleConnection(conn)
+		go handleConnection(conn)
 	}
 }
 
-
-func handleConnection(conn *net.TCPConn) (ex error){
+func handleConnection(conn *net.TCPConn) error {
 	var message = make(chan []byte, WRKR_COUNT)
-	go func(){
+	var erCh = make(chan error)
+	go func() {
 		read := make([]byte, MAX_COMM_SIZE)
 		_, err := conn.Read(read)
 		message <- read
-		if(!errorHandler(err)){
-			fmt.Fprintf(os.Stdout, "The current request generateed error!\n%s\n%T - %s", string(read), err, err)
-			ex = err
-			return
-		}
+		erCh <- err
 	}()
 	buf := <-message
-	phase, err := handlePhase(conn, buf)
-	if(!errorHandler(err)){
-		fmt.Fprintf(os.Stdout, "There was an error communicating during phase %d connecting with %s", phase, conn.RemoteAddr().String())
+	err := <-erCh
+	if err != nil {
+		e.HandlErr("WARN", "This request generateed an error!\n"+string(buf), err)
+	}
+	phase, err := checkConn(conn, buf)
+	if err != nil {
+		e.HandlErr("WARN", "There was an error communicating during phase "+string(phase)+" connecting with "+conn.RemoteAddr().String()+"\n", err)
 	}
 	conn.Close()
-	ex = err
-	return
+	//TODO Managing conncetion
+	return err
 }
 
-
-
-func handlePhase(conn *net.TCPConn, buf []byte) (phase string, ex error){
+func checkConn(conn *net.TCPConn, buf []byte) (phase string, ex error) {
 	phase = string(buf)
 	fmt.Fprintf(os.Stdout, "PHASE_RECV: %s\n", phase)
 	conn.Write([]byte(RESP_OK))
