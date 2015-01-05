c197078f145cf8fbad358b061b959ea18a6797d4
inge4pres <fgualazzi@gmail.com>
2015-01-05 12:48:51 +0100

    Finished the basic implementation of handshake

--- tcpconn/tcpserver.go
+++ tcpconn/tcpserver.go
@@ -5,6 +5,8 @@ import (
 	ex "inge4pres/goutils/handlerr"
 	"net"
 	"os"
+	json "encoding/json"
+	"errors"
 )
 
 var e = ex.Herr{LogFile: "./tcpserver.err"}
@@ -29,32 +31,43 @@ func TcpServerListener(conntype, host, port string) error {
 	}
 }
 
-func handleConnection(conn *net.TCPConn) error {
+func handleConnection(conn *net.TCPConn) {
+	comm, err := StartConn(conn)
+	//TODO The rest of protocol specification
+	fmt.Fprintf(os.Stdout, "Error during connection!\n%T\n%s\nClient info:\n%s\n%s", err, err, comm.LAddr, comm.LHost)
+}
+
+func StartConn(conn *net.TCPConn) (comm *TCPCommand, ex error) {
 	var message = make(chan []byte, WRKR_COUNT)
 	var erCh = make(chan error)
-	go func() {
+	for {
 		read := make([]byte, MAX_COMM_SIZE)
-		_, err := conn.Read(read)
-		message <- read
-		erCh <- err
-	}()
+		b, err := conn.Read(read)
+		message<-read
+		erCh<-err
+		if b == 0 {
+			break
+		}
+	}
 	buf := <-message
 	err := <-erCh
 	if err != nil {
 		e.HandlErr("WARN", "This request generateed an error!\n"+string(buf), err)
 	}
-	phase, err := checkConn(conn, buf)
+	json.Unmarshal(buf, comm)
+	err = startHandShake(conn, comm.Phase)
 	if err != nil {
-		e.HandlErr("WARN", "There was an error communicating during phase "+string(phase)+" connecting with "+conn.RemoteAddr().String()+"\n", err)
+		e.HandlErr("WARN", "There was an error communicating during phase "+string(comm.Phase)+" connecting with "+conn.RemoteAddr().String()+"\n", err)
 	}
-	conn.Close()
-	//TODO Managing conncetion
-	return err
+/*	Implements only the initial handshake
+	Managing the rest of portocol after handshake must be done in specific way
+*/	return comm, err
 }
 
-func checkConn(conn *net.TCPConn, buf []byte) (phase string, ex error) {
-	phase = string(buf)
-	fmt.Fprintf(os.Stdout, "PHASE_RECV: %s\n", phase)
-	conn.Write([]byte(RESP_OK))
-	return
+func startHandShake(conn *net.TCPConn, phase int) error {
+	if phase == PHASE_CONN {
+		conn.Write([]byte(RESP_OK))
+		return nil
+	}
+	return errors.New("Could not start handshake with client, protocol phase mismatch")
 }
