56219fe224d3435d3c3ed29fc93535602097e4c7
inge4pres <fgualazzi@gmail.com>
2015-01-08 23:23:07 +0100

    Updated exmaple client

--- tcpconn/example/client.go
+++ tcpconn/example/client.go
@@ -18,18 +18,22 @@ func main(){
 	flag.Parse()
 
 	comm := tconn.TCPCommand{Proto:*proto,OS:runtime.GOOS,RAddr:*host,RPort:*port,Phase:tconn.PHASE_CONN}
-	fmt.Printf("starting connection\n")
 	conn, err := comm.Connect()
 	if err != nil {
 		fmt.Fprintf(os.Stdout, "Error initiating the connection\n%T\n%s", err, err)
 	}
-	fmt.Printf("posting command...\n")
 	err = comm.PostCommand(conn)
 	if err != nil {
 		fmt.Fprintf(os.Stdout, "Error posting the command\n%T\n%s", err, err)
 	}
-	fmt.Printf("Waiting for response from server....")
-	comm.ReceiveResp(conn)
+	fmt.Println("Waiting for response from server....")
+	data, err := comm.ReceiveResp(conn)
+	if  err != nil {
+		fmt.Println("Error receiving response: ")
+		fmt.Println(err)
+	}
+	fmt.Println("DATA: ")
+	fmt.Println(data)
 	comm.Disconnect(conn)
 	return
 }
